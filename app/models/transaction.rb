d04393e8bee9ae0cc6c18cf757b41471398d9b54
Mostafa Elhefnawy <moselhefnawy@gmail.com>
2013-12-19 13:47:24 +0200

    validate_all_accounts is working

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -13,47 +13,60 @@ class Transaction < ActiveRecord::Base
       before_update :check_amount_exist
       #validates :check_amount_exist
 
-      def check_amount_exist
-          if Account.find_by_id(self.from_account_id).a_amount >= self.t_amount
-             true
-          else
-	     
-	     errors[:base] << Account.find_by_id(self.from_account_id).a_name + " Account does not have enough balance it only have = " + Account.find_by_id(self.from_account_id).a_amount.to_s
- 	     false 
+     def check_amount_exist
+		if Account.find_by_id(self.from_account_id).a_amount >= self.t_amount
+            true
+		else
+			errors[:base] << Account.find_by_id(self.from_account_id).a_name + " Account does not have enough balance it only have = " + Account.find_by_id(self.from_account_id).a_amount.to_s
+			false 
              #return "The account does not have enough amount"
-          end
-      end
+        end
+	end
 
-def validate_all_accounts
-          @asset_amount = 0
-          Account.select(:a_amount).where("a_type = 'Asset'").each do |asset|
-		@asset_amount = asset.a_amount + @asset_amount
+	 
+	def validate_all_accounts
+        @asset_amount = 0
+        Account.select(:a_amount).where("a_type = 'Asset'").each do |asset|
+			@asset_amount = asset.a_amount + @asset_amount
               
-          end
-	  @liability_amount = 0
-          Account.select(:a_amount).where("a_type = 'Liability'").each do |liability|
-		@liability_amount = liability.a_amount + @liability_amount
+        end
+		puts @asset_amount.to_s + "ooooooooooooooooooooooooooo"
+		
+		@liability_amount = 0
+        Account.select(:a_amount).where("a_type = 'Liability'").each do |liability|
+			@liability_amount = liability.a_amount + @liability_amount
               
-          end
-          @expense_amount = 0
-          Account.select(:a_amount).where("a_type = 'Expense'").each do |expense|
-		@expense_amount = expense.a_amount + @expense_amount
-              
-          end
-          @equity_amount = 0
-          Account.select(:a_amount).where("a_type = 'Equity'").each do |equity|
-		@equity_amount = equity.a_amount + @equity_amount
-              
-          end       
+        end
+		puts @liability_amount.to_s + "ooooooooooooooooooooooooooo"
+        
+		@expense_amount = 0
+        Account.select(:a_amount).where("a_type = 'Expense'").each do |expense|
+			@expense_amount = expense.a_amount + @expense_amount
+        end
+		puts @expense_amount.to_s + "ooooooooooooooooooooooooooo"
+		
+        @equity_amount = 0
+		@capital_equity_amount = 0
+		@other_equity_amount = 0
+        Account.select(:a_name,:a_amount).where("a_type = 'Equity'").each do |equity|
+			if equity.a_name == "Capital"
+				@capital_equity_amount=equity.a_amount
+			else	
+				@other_equity_amount += equity.a_amount 
+			end
+        end       
+		@equity_amount = @capital_equity_amount - @other_equity_amount
+		puts @equity_amount.to_s + "ooooooooooooooooooooooooooo"
        
 
-	  if   @asset_amount - @liability_amount + @expense_amount - @equity_amount ==0
-          true
-          else
+		if   (@asset_amount - @liability_amount + @expense_amount - @equity_amount) ==0
+			true
+        else
 	     
-	     errors[:base] << " The Transaction validation fails "
- 	     false 
-             #return "The account does not have enough amount"
-          end
-      end
+			errors[:base] << " The Transaction validation fails "
+			p (@asset_amount - @liability_amount + @expense_amount - @equity_amount)
+			false 
+				 #return "The account does not have enough amount"
+        end
+    end
 end
