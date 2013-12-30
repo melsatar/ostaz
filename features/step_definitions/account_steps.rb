5090f1997bf49779c6a168a734aa5fddf07164aa
melsatar <melsatar@ubuntu.(none)>
2013-12-30 21:54:37 +0200

    cleaning code

--- features/step_definitions/account_steps.rb
+++ features/step_definitions/account_steps.rb
@@ -1,9 +1,6 @@
 require 'simplecov'
 SimpleCov.start
 
-
-
-
 Given(/^Account type "(.*?)" exist$/) do |arg1|
 
    arg1 = ['Expense','Liability','Asset','Equity']
--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -55,6 +55,7 @@ end
 When /^(?:|I )press "([^"]*)"$/ do |button|
   click_button(button)
 end
+
 Given /^I am a new, authenticated user$/ do
   email = 'testing@man.net'
   password = 'secretpass'
@@ -78,27 +79,11 @@ When /^(?:|I )fill in "([^"]*)" for "([^"]*)"$/ do |value, field|
   fill_in(field, :with => value)
 end
 
-# Use this to fill in an entire form with data from a table. Example:
-#
-#   When I fill in the following:
-#     | Account Number | 5002       |
-#     | Expiry date    | 2009-11-01 |
-#     | Note           | Nice guy   |
-#     | Wants Email?   |            |
-#
-# TODO: Add support for checkbox, select or option
-# based on naming conventions.
-#
 When /^(?:|I )fill in the following:$/ do |fields|
   fields.rows_hash.each do |name, value|
     When %{I fill in "#{name}" with "#{value}"}
   end
 end
-=begin
-When /^(?:|I )select "([^"]*)" from "([^"]*)"$/ do |value, field|
-  select(value, :from => field)
-end
-=end
 
 When /^(?:|I )check "([^"]*)"$/ do |field|
   check(field)
